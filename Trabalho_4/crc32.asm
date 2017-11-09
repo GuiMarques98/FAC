63260af27c82bfb90e2c7319de646ba58670b8fc
GuiMarques98 <guilherme.marques.moreira@gmail.com>
2017-11-09 21:25:45 -0200

    Adicionando estrutura do crc

--- Trabalho_4/crc32.asm
+++ Trabalho_4/crc32.asm
@@ -4,7 +4,14 @@ msg1:	.asciiz "Digite a mensagem para ser convertida para crc32 (maximo 16 carac
 quebra: .ascii 	"\n"
 .text
 main:
-
+	jal le_string	# Leitura de string
+	la $a0, buffer
+	addi $v0, $0, 4
+	syscall
+	jal calc_crc32
+calc_main:
+	move $a0, $v0	# Carrega a resposta do calculo do crc pra printar
+	jal imprime_saida	# Imprime a saida	
 	addi $v0, $0, 10
 	syscall		# Finaliza programa
 endmain:
@@ -19,11 +26,75 @@ le_string:
 			# contando com o terminador
 	addi $v0, $0, 8	
 	syscall		# Le a string
+end_le_string:
 
 reverse:
+	move $s5, $a0	# Inicializando o $s5 com o valor da 
+	xor $s1, $s1, $s1 # contador 
+	xor $s6, $s6, $s6 # rotação 
 
+	for_reverse:
+		#add $s0, $zero .1 # mascara	
+		add $s0, $zero, 1
+		and $s0, $s0, $s5 # pega o último bit
+		or $s6, $s6, $s0 # salva no registrador $s6
+		add $s7, $zero ,$s6
+		srl $s5, $s5, 1
+		sll $s6, $s6, 1
+		add $s1, $s1, 1
+	bne $s1, 32, for_reverse
+	
+	move $v0, $s6	# Move a resposta para o registrador de retorno 
+	jr $ra	# Retorna
+end_reverse:
 
 calc_crc32:
+	# As proximas 2 linhas zeram os registrador
+	move $t9, $0	# Contador para a mensagem
+	move $t7, $0	# Registrador para o byte
+	addi $t6, $0, 0xffffffff	# Mascata 0xFFFFFFFF do CRC		
+	addi $t5, $0, 0x04c11db7	# Mascara do or 0x04C11DB7
+	la $t4, buffer	# Carrega endereço da paralvra lida
+
+	while:
+		lb $t7, 0($t4)	# Carrega o byte da mensagem para o registrador
+		beqz $t7, exit_while	# Verifica se e o terminador da string
+		lb $t0, quebra	# Carrega a quebra de linha
+		beq $t7, $t0, exit_while	#Verifica se e quebra de linha
+		# Funcao de reverse
+		move $a0, $t7
+		jal reverse
+		move $t7, $v0	# Move a resposta de $v0 para $t7
+		
+		move $t8, $0	# Zera contador do for
+		for_crc:
+			bgt $t8, 7, end_for_crc	# Se for maior do que 7 finaliza o for
+			if:
+				xor $t3, $t6, $t7	# byte xor crc mascara
+				bgtz $t3, else	# Verifica se e menor que zero
+				beqz $t3, else	# Verifica se e igual a zero
+				sll $t6, $t6, 1	# Shift pra esquerda do crc
+				xor $t6, $t6, $t5	#  crc xor 0x04C11DB7
+				j end_if
+			else:
+				sll $t6, $t6, 1	# Shift pra esquerda do crc				
+			end_if:
+			sll $t7, $t7, 1	# Shift pra esquerda no byte da mensagem
+			addi $t8, $t8, 1	# Adiciona 1 ao contador
+			j for_crc	# retorna pro inicio do for
+		end_for_crc:
+		addi $t4, $t4, 1	# Adiciona 8 para ler o proximo byte da mensagem
+		j while	# Retorna para o inicio do while
+	exit_while:
+	not $t6, $t6	# Faz um not crc
+	# Funcao de reverse
+	move $a0, $t6
+	jal reverse
+	j calc_main	# Retorna a funcao com o crc revertido
+end_calc_crc_32:
 
 
 imprime_saida:
+	addi $v0, $0, 34	# Carrega numero referente ao print em hexadecimal
+	syscall
+end_imprime_saida:
--- Trabalho_4/teste.c
+++ Trabalho_4/teste.c
@@ -16,24 +16,35 @@ unsigned int crc32a(unsigned char *message) {
 	i = 0;
 	crc = 0xFFFFFFFF;
 	while (message[i] != 0) {
-	byte = message[i];            // Get next byte.
-	byte = reverse(byte);         // 32-bit reversal.
-	for (j = 0; j <= 7; j++) {    // Do eight times.
-		if ((int)(crc ^ byte) < 0)
-			crc = (crc << 1) ^ 0x04C11DB7;
-		else 
-			crc = crc << 1;
-	byte = byte << 1;          // Ready next msg bit.
-	}
-	i = i + 1;
+		byte = message[i];            // Get next byte.
+		byte = reverse(byte);         // 32-bit reversal.
+		for (j = 0; j <= 7; j++) {    // Do eight times.
+			if ((int)(crc ^ byte) < 0)
+				crc = (crc << 1) ^ 0x04C11DB7;
+			else 
+				crc = crc << 1;
+			byte = byte << 1;          // Ready next msg bit.
+		}
+		i = i + 1;
 	}
 	return reverse(~crc);
 }
 
 int main(int argc, char const *argv[]) {
 	char in[16];
-	printf("unsigned int = %lu ; unsigned = %lu\n", sizeof(unsigned int), sizeof(unsigned));
-	scanf("%[^\n]", in);
-	printf("%x\n", crc32a(in));
+	unsigned a = 1;
+	printf("unsigned int = %x ; reverse = %x\n", a, reverse(a));
+	//scanf("%[^\n]", in);
+	//printf("%x\n", crc32a(in));
 	return 0;
 }
+
+
+/*
+Questao 2: Perceba que o programa mantém registros excluídos no vetor e mantém um status no registro de cada 
+aluno.  Porque  esse  campo  foi  necessário?  Responda  procurando  avaliar  vantagens  e  desvantagens  dessa estratégia. 
+Você teria uma solução mais interessante em relação ao que foi proposto?
+Para que se saiba se o elemento foi excluido ou não do vetor na hora de printar. As vantagens são na hora de escrever o 
+algoritmo de exclusão do elemento q se faz mais facil, a desvantagem é na memória pois todos os elementos inseridos continuam
+na memória do pc independente se foi excluido ou não. Sim, excluiria o elemento do vetor.
+*/
